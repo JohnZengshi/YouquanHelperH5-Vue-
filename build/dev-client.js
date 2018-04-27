6bccb1bde92b14110ba83a99727b7e0bc06e69e0
heidanchaofanhei <woaiwojia8664>
2018-04-27 18:35:17 +0800

    基础环境配置

---
+++ build/dev-client.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+// 在不支持事件源的浏览器里添加填充策略支持
+require('eventsource-polyfill')
+var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
+
+hotClient.subscribe(function (event) {
+  if (event.action === 'reload') {
+    window.location.reload()
+  }
+})
