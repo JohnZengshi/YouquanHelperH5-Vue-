11257e71233d387d768fab7007fa63467844960b
heidanchaofanhei <woaiwojia8664>
2018-05-02 18:21:00 +0800

    抛弃了vue-resoure，增加了goodsDetail视图，导入了公共样式和初始化rem,配置了goodsDetail路由

--- src/main.js
+++ src/main.js
@@ -2,31 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
+
 import App from './App';
-// import goods from './components/goods/goods.vue';
-// import ratings from './components/ratings/ratings.vue';
-// import seller from './components/seller/seller.vue';
-// import 'common/stylus/index.styl';
-// 安装 "VueRouter"这个插件
-/* eslint-disable no-new */
+import goodsDetail from './view/goodsDetail';
+import './common/stylus/index.styl';
+import './common/js/init.js';
+// 通过全局方法 Vue.use() 使用插件
 Vue.use(VueRouter);
-Vue.use(VueResource);
 let routes = [{
-  path: '/',
-  name: 'index',
-  component: App
-//   children: [{
-//     path: '/goods',
-//     component: goods
-//   }, {
-//     path: '/ratings',
-//     component: ratings
-//   }, {
-//     path: '/seller',
-//     component: seller
-//   }]
-}];
+    path: '/',
+    component: App
+  },
+  {
+    path: '/goodsDetail',
+    component: goodsDetail
+  }
+];
 let router = new VueRouter({
   'linkActiveClass': 'active',
   routes // （缩写）相当于 routes: routes
