19d2cb1f6d2418ff73e717035cf35069671c42e7
JOHNPC\JohnZeng <1351009316@qq.com>
2018-05-20 17:22:38 +0800

    简化了main.js，抽出router

--- src/main.js
+++ src/main.js
@@ -1,29 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-
 import App from './App';
-import goodsDetail from './view/goodsDetail';
-import './common/stylus/index.styl';
-import './common/js/init.js';
-// 通过全局方法 Vue.use() 使用插件
-Vue.use(VueRouter);
-let routes = [{
-    path: '/',
-    component: App
-  },
-  {
-    path: '/goodsDetail',
-    component: goodsDetail
+import router from './router';
+Vue.config.productionTip = false;
+const app = new Vue({
+  router,
+  template: '<App/>',
+  components: {
+    App
   }
-];
-let router = new VueRouter({
-  'linkActiveClass': 'active',
-  routes // （缩写）相当于 routes: routes
-});
-let app = new Vue({
-  router
 }).$mount('#app');
-// router.push('/goods');
 export default app;
